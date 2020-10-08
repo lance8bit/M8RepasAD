2c4cff921adcd831d23e34c05a0139aadef034e3
Stephen <ogonzalez@iesjoandaustria.org>
2020-10-08 17:57:40 +0200

    Implemented Recycler View

--- app/src/main/java/com/example/m8repasad/ui/ListIncidence/ListIncidenceFragment.java
+++ app/src/main/java/com/example/m8repasad/ui/ListIncidence/ListIncidenceFragment.java
@@ -25,8 +25,6 @@ public class ListIncidenceFragment extends Fragment {
 
     private ListIncidenceViewModel mViewModel;
     private RecyclerView recyclerView;
-    private RecyclerView.LayoutManager layoutManager;
-    private RecyclerView.Adapter mAdapter;
 
     public static ListIncidenceFragment newInstance() {
         return new ListIncidenceFragment();
@@ -37,63 +35,60 @@ public class ListIncidenceFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         mViewModel = ViewModelProviders.of(this).get(ListIncidenceViewModel.class);
         View root = inflater.inflate(R.layout.list_incidence_fragment, container, false);
+        recyclerView = root.findViewById(R.id.recyclerIncidences);
 
-        ArrayList<Incidencia> reto = singletonIncidencias.getNewInstance().getListIncidencias();
+            //FOR SINGLETON CHECK
+            ArrayList<Incidencia> reto = singletonIncidencias.getNewInstance().getListIncidencias();
 
-        for(int x = 0; x < reto.size(); x++){
-            Log.i("RECY", "TITLE: " + reto.get(x).getTitleIncidencia());
-        }
+            for(int x = 0; x < reto.size(); x++){
+                Log.i("RECY", "TITLE: " + reto.get(x).getTitleIncidencia());
+            }
 
-        recyclerView = (RecyclerView) root.findViewById(R.id.recyclerIncidences);
-        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager rLManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(rLManager);
 
-        layoutManager = new LinearLayoutManager(getActivity());
-        recyclerView.setLayoutManager(layoutManager);
-
-        mAdapter = new MyAdapter();
-        recyclerView.setAdapter(mAdapter);
+        MainAdapter rAdapter = new MainAdapter();
+        recyclerView.setAdapter(rAdapter);
 
         return root;
     }
 
+    public class MainAdapter extends RecyclerView.Adapter<ViewHolder>{
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(ListIncidenceViewModel.class);
-        // TODO: Use the ViewModel
-    }
+        private ArrayList<Incidencia> arrayListIncidencias;
 
-    public class MyAdapter extends RecyclerView.Adapter<RecyclerView.MyViewHolder> {
-
-        private String[] mDataset;
-
-        public static class MyViewHolder extends RecyclerView.ViewHolder{
-
-            public MyViewHolder(@NonNull View itemView) {
-                super(itemView);
-            }
+        public MainAdapter(){
+            arrayListIncidencias = singletonIncidencias.getNewInstance().getListIncidencias();
         }
 
         @NonNull
         @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-
-            return null;
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+            View nVistaHolder = layoutInflater.inflate(R.layout.view_rv_single_incidence, parent, false);
+            return new ViewHolder(nVistaHolder);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull RecyclerView.MyViewHolder holder, int position) {
-
+        public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+            viewHolder.titleInc.setText(arrayListIncidencias.get(position).getTitleIncidencia());
+            viewHolder.priorityInc.setText(arrayListIncidencias.get(position).getPriorityIncidencia());
         }
-
 
         @Override
         public int getItemCount() {
-            return 0;
+            return arrayListIncidencias.size();
         }
     }
 
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView titleInc, priorityInc;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleInc = (TextView) itemView.findViewById(R.id.titleIncidence);
+            priorityInc = (TextView) itemView.findViewById(R.id.priorityIncidence);
+        }
+    }
 
 }
